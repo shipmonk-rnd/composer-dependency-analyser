765d8130e3833052fae96f70689c36f3825302a7
Jan Nedbal <janedbal@gmail.com>
2024-01-10 13:29:25 +0100

    Validate provided cli args (#17)

---
+++ tests/CliTest.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\ComposerDependencyAnalyser;
+
+use PHPUnit\Framework\TestCase;
+use ShipMonk\ComposerDependencyAnalyser\Exception\InvalidCliException;
+
+class CliTest extends TestCase
+{
+
+    /**
+     * @param list<string> $argv
+     * @dataProvider validateArgvDataProvider
+     */
+    public function testValidateArgv(?string $expectedExceptionMessage, array $argv): void
+    {
+        if ($expectedExceptionMessage === null) {
+            $this->expectNotToPerformAssertions();
+        } else {
+            $this->expectException(InvalidCliException::class);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        }
+
+        $cli = new Cli();
+        $cli->validateArgv(__DIR__, $argv);
+    }
+
+    /**
+     * @return iterable<string, array{string|null, list<string>}>
+     */
+    public function validateArgvDataProvider(): iterable
+    {
+        yield 'unknown long option' => [
+            'Unknown option --unknown, see --help',
+            ['bin/script.php', '--unknown']
+        ];
+
+        yield 'unknown short option' => [
+            'Unknown option -u, see --help',
+            ['bin/script.php', '-u']
+        ];
+
+        yield 'unknown argument' => [
+            'Unknown argument unknown, see --help',
+            ['bin/script.php', 'unknown']
+        ];
+
+        yield 'path passed' => [
+            'Cannot pass paths (vendor) to analyse as arguments, use --config instead.',
+            ['bin/script.php', 'vendor']
+        ];
+
+        yield 'valid bool options' => [
+            null,
+            ['bin/script.php', '--help', '--verbose', '--ignore-shadow-deps', '--ignore-unused-deps', '--ignore-dev-in-prod-deps', '--ignore-unknown-classes']
+        ];
+
+        yield 'valid options with values' => [
+            null,
+            ['bin/script.php', '--composer-json', '../composer.json', '--verbose']
+        ];
+
+        yield 'valid options with values using "' => [
+            null,
+            ['bin/script.php', '--composer-json', '"../composer.json"', '--verbose']
+        ];
+
+        yield 'valid options with values using =' => [
+            null,
+            ['bin/script.php', '--composer-json=../composer.json', '--verbose']
+        ];
+
+        yield 'valid options with values using = and "' => [
+            null,
+            ['bin/script.php', '--composer-json="../composer.json"', '--verbose']
+        ];
+
+        yield 'missing argument for option' => [
+            'Missing argument for --composer-json, see --help',
+            ['bin/script.php', '--composer-json']
+        ];
+
+        yield 'missing argument for option, next option is valid' => [
+            'Missing argument for --composer-json, see --help',
+            ['bin/script.php', '--composer-json', '--verbose']
+        ];
+
+        yield 'missing argument for option with =' => [
+            'Missing argument value in --composer-json=, see --help',
+            ['bin/script.php', '--composer-json=']
+        ];
+    }
+
+}
