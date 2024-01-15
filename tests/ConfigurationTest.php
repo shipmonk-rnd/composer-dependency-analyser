eca9f291e685b00db25a7b51fbd0df56dd2e7121
Jan Nedbal <janedbal@gmail.com>
2024-01-15 17:12:48 +0100

    Ensure ignored paths are realpathed

---
+++ tests/ConfigurationTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\ComposerDependencyAnalyser;
+
+use PHPUnit\Framework\TestCase;
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+class ConfigurationTest extends TestCase
+{
+
+    public function testPathsWithIgnore(): void
+    {
+        $configuration = new Configuration();
+        $configuration->ignoreErrorsOnPath(__DIR__ . '/app/../', [ErrorType::SHADOW_DEPENDENCY]);
+        $configuration->ignoreErrorsOnPackageAndPath('vendor/package', __DIR__ . '/../tests/app', [ErrorType::SHADOW_DEPENDENCY]);
+
+        self::assertEquals(
+            [
+                __DIR__,
+                __DIR__ . '/app',
+            ],
+            $configuration->getPathsWithIgnore()
+        );
+    }
+
+}
