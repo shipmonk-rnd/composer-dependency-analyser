aa6facb26ad71d2ce66678489761819ecaa88197
Jan Nedbal <janedbal@gmail.com>
2024-02-16 09:39:18 +0100

    Add more paths to self check

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -4,4 +4,6 @@ use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
+    ->addPathToScan(__FILE__, true)
+    ->addPathToScan(__DIR__ . '/bin', false)
     ->ignoreErrorsOnPath(__DIR__ . '/tests/data', [ErrorType::UNKNOWN_CLASS]);
