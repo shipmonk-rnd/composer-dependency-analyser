11ec0a98abb7fd4c4f5e131226d412276dc618cc
Jan Nedbal <janedbal@gmail.com>
2024-03-15 15:16:36 +0100

    phpcs.xml: scan all php files (#99)

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,6 @@
 <?php declare(strict_types = 1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
-use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToScan(__FILE__, true)
