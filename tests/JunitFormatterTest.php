ccb1e5c70e254110ee16c4fb7d15ce085cedea6d
Jan Nedbal <janedbal@gmail.com>
2024-04-08 13:42:58 +0200

    JunitFormatterTest: pretty print xml (#116)

--- tests/JunitFormatterTest.php
+++ tests/JunitFormatterTest.php
@@ -3,6 +3,7 @@
 namespace ShipMonk\ComposerDependencyAnalyser;
 
 use Closure;
+use DOMDocument;
 use PHPUnit\Framework\TestCase;
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
@@ -13,12 +14,15 @@ use ShipMonk\ComposerDependencyAnalyser\Result\SymbolUsage;
 use function ob_get_clean;
 use function ob_start;
 use function str_replace;
+use function trim;
+use const LIBXML_NOEMPTYTAG;
 
 class JunitFormatterTest extends TestCase
 {
 
     public function testPrintResult(): void
     {
+        // editorconfig-checker-disable
         $formatter = new JunitFormatter('/app', new Printer());
 
         $noIssuesOutput = $this->captureAndNormalizeOutput(static function () use ($formatter): void {
@@ -29,15 +33,23 @@ class JunitFormatterTest extends TestCase
         });
 
         $expectedNoIssuesOutput = <<<'OUT'
-<?xml version="1.0" encoding="UTF-8"?><testsuites></testsuites>
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuites></testsuites>
 OUT;
 
         $expectedNoIssuesButWarningsOutput = <<<'OUT'
-<?xml version="1.0" encoding="UTF-8"?><testsuites><testsuite name="unused-ignore" failures="1"><testcase name="shadow-dependency"><failure>'shadow-dependency' was globally ignored, but it was never applied.</failure></testcase></testsuite></testsuites>
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuites>
+  <testsuite name="unused-ignore" failures="1">
+    <testcase name="shadow-dependency">
+      <failure>'shadow-dependency' was globally ignored, but it was never applied.</failure>
+    </testcase>
+  </testsuite>
+</testsuites>
 OUT;
 
-        self::assertSame($this->normalizeEol($expectedNoIssuesOutput), $noIssuesOutput);
-        self::assertSame($this->normalizeEol($expectedNoIssuesButWarningsOutput), $noIssuesButUnusedIgnores);
+        self::assertSame($this->normalizeEol($expectedNoIssuesOutput), $this->prettyPrintXml($noIssuesOutput));
+        self::assertSame($this->normalizeEol($expectedNoIssuesButWarningsOutput), $this->prettyPrintXml($noIssuesButUnusedIgnores));
 
         $analysisResult = new AnalysisResult(
             10,
@@ -77,14 +89,85 @@ OUT;
         });
 
         $expectedRegularOutput = <<<'OUT'
-<?xml version="1.0" encoding="UTF-8"?><testsuites><testsuite name="unknown classes" failures="1"><testcase name="Unknown\Thing"><failure>in app/init.php:1093</failure></testcase></testsuite><testsuite name="unknown functions" failures="1"><testcase name="Unknown\function"><failure>in app/foo.php:51</failure></testcase></testsuite><testsuite name="shadow dependencies" failures="2"><testcase name="shadow/another"><failure>e.g. Another\Controller in src/bootstrap.php:173</failure></testcase><testcase name="shadow/package"><failure>e.g. Forth\Provider in src/bootstrap.php:873 (+ 6 more)</failure></testcase></testsuite><testsuite name="dev dependencies in production code" failures="1"><testcase name="some/package"><failure>e.g. Another\Command in src/ProductGenerator.php:28</failure></testcase></testsuite><testsuite name="prod dependencies used only in dev paths" failures="1"><testcase name="misplaced/package"><failure></failure></testcase></testsuite><testsuite name="unused dependencies" failures="1"><testcase name="dead/package"><failure></failure></testcase></testsuite></testsuites>
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuites>
+  <testsuite name="unknown classes" failures="1">
+    <testcase name="Unknown\Thing">
+      <failure>in app/init.php:1093</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="unknown functions" failures="1">
+    <testcase name="Unknown\function">
+      <failure>in app/foo.php:51</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="shadow dependencies" failures="2">
+    <testcase name="shadow/another">
+      <failure>e.g. Another\Controller in src/bootstrap.php:173</failure>
+    </testcase>
+    <testcase name="shadow/package">
+      <failure>e.g. Forth\Provider in src/bootstrap.php:873 (+ 6 more)</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="dev dependencies in production code" failures="1">
+    <testcase name="some/package">
+      <failure>e.g. Another\Command in src/ProductGenerator.php:28</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="prod dependencies used only in dev paths" failures="1">
+    <testcase name="misplaced/package">
+      <failure></failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="unused dependencies" failures="1">
+    <testcase name="dead/package">
+      <failure></failure>
+    </testcase>
+  </testsuite>
+</testsuites>
 OUT;
         $expectedVerboseOutput = <<<'OUT'
-<?xml version="1.0" encoding="UTF-8"?><testsuites><testsuite name="unknown classes" failures="1"><testcase name="Unknown\Thing"><failure>app/init.php:1093</failure></testcase></testsuite><testsuite name="unknown functions" failures="1"><testcase name="Unknown\function"><failure>app/foo.php:51</failure></testcase></testsuite><testsuite name="shadow dependencies" failures="2"><testcase name="shadow/another"><failure>Another\Controller\n  src/bootstrap.php:173</failure></testcase><testcase name="shadow/package"><failure>Forth\Provider\n  src/bootstrap.php:873\nShadow\Comparator\n  src/Printer.php:25\nShadow\Utils\n  src/Utils.php:19\n  src/Utils.php:22\n  src/Application.php:128\n  + 1 more\n  + 1 more symbol</failure></testcase></testsuite><testsuite name="dev dependencies in production code" failures="1"><testcase name="some/package"><failure>Another\Command\n  src/ProductGenerator.php:28</failure></testcase></testsuite><testsuite name="prod dependencies used only in dev paths" failures="1"><testcase name="misplaced/package"><failure></failure></testcase></testsuite><testsuite name="unused dependencies" failures="1"><testcase name="dead/package"><failure></failure></testcase></testsuite></testsuites>
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuites>
+  <testsuite name="unknown classes" failures="1">
+    <testcase name="Unknown\Thing">
+      <failure>app/init.php:1093</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="unknown functions" failures="1">
+    <testcase name="Unknown\function">
+      <failure>app/foo.php:51</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="shadow dependencies" failures="2">
+    <testcase name="shadow/another">
+      <failure>Another\Controller\n  src/bootstrap.php:173</failure>
+    </testcase>
+    <testcase name="shadow/package">
+      <failure>Forth\Provider\n  src/bootstrap.php:873\nShadow\Comparator\n  src/Printer.php:25\nShadow\Utils\n  src/Utils.php:19\n  src/Utils.php:22\n  src/Application.php:128\n  + 1 more\n  + 1 more symbol</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="dev dependencies in production code" failures="1">
+    <testcase name="some/package">
+      <failure>Another\Command\n  src/ProductGenerator.php:28</failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="prod dependencies used only in dev paths" failures="1">
+    <testcase name="misplaced/package">
+      <failure></failure>
+    </testcase>
+  </testsuite>
+  <testsuite name="unused dependencies" failures="1">
+    <testcase name="dead/package">
+      <failure></failure>
+    </testcase>
+  </testsuite>
+</testsuites>
 OUT;
 
-        self::assertSame($this->normalizeEol($expectedRegularOutput), $regularOutput);
-        self::assertSame($this->normalizeEol($expectedVerboseOutput), $verboseOutput);
+        self::assertSame($this->normalizeEol($expectedRegularOutput), $this->prettyPrintXml($regularOutput));
+        self::assertSame($this->normalizeEol($expectedVerboseOutput), $this->prettyPrintXml($verboseOutput));
+        // editorconfig-checker-enable
     }
 
     /**
@@ -100,6 +183,19 @@ OUT;
     private function normalizeEol(string $string): string
     {
         return str_replace("\r\n", "\n", $string);
+    }
+
+    private function prettyPrintXml(string $inputXml): string
+    {
+        $dom = new DOMDocument();
+        $dom->preserveWhiteSpace = false;
+        $dom->formatOutput = true;
+        $dom->loadXML($inputXml);
+
+        $outputXml = $dom->saveXML(null, LIBXML_NOEMPTYTAG);
+        self::assertNotFalse($outputXml);
+
+        return trim($outputXml);
     }
 
 }
